c4468cc1c8b06f4607560b014032de4a5400a977
shengchao wu <shengchao.wu@lombardrisk.com>
2018-09-04 18:29:27 +0800

    synchronize code

--- src/main/java/springAnnotation/config/MyProfileConfiguration.java
+++ src/main/java/springAnnotation/config/MyProfileConfiguration.java
@@ -1,7 +1,6 @@
 package springAnnotation.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
